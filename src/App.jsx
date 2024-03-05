3325e1cfaefc702c3ec5c2b6a1c6dd02da5d7174
oyasuminaaa <150317826+oyasuminaaa@users.noreply.github.com>
2024-03-05 05:51:23 +0000

    aaad

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,40 @@ function App() {
 
   return (
     <>
-      <AnimatedCursor
-  color="#blue"
-  innerSize={8}
-  outerSize={35}
-  innerScale={1}
-  outerScale={1.7}
-  outerAlpha={0}
-  outerStyle={{
-    mixBlendMode: 'exclusion'
-  }}
-/>
+     <div className="App">
+    <AnimatedCursor
+      innerSize={8}
+      outerSize={8}
+      color='193, 11, 111'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link',
+        {
+          target: '.custom',
+          options: {
+            innerSize: 12,
+            outerSize: 12,
+            color: '255, 255, 255',
+            outerAlpha: 0.3,
+            innerScale: 0.7,
+            outerScale: 5
+          }
+        }
+      ]}
+    />
+    </div>
       <Navbar />
       <Hero />
       <Skills/>
