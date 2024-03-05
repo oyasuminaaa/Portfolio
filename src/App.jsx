5e2a1e1e50cd9c527b9697032daa9e78ded9ea92
oyasuminaaa <150317826+oyasuminaaa@users.noreply.github.com>
2024-03-05 05:35:19 +0000

    zx

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,6 @@ function App() {
 
   return (
     <>
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={35}
-        innerScale={1}
-        outerScale={2}
-        outerAlpha={0}
-        hasBlendMode={true}
-        innerStyle={{
-          backgroundColor: 'var(--cursor-color)'
-        }}
-        outerStyle={{
-          border: '3px solid var(--cursor-color)'
-        }}
-      />
       <Navbar />
       <Hero />
       <Skills/>
