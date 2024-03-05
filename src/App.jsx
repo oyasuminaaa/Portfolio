6e5a6c6e1d3a5e45973676afee840e6e6d1877c8
oyasuminaaa <150317826+oyasuminaaa@users.noreply.github.com>
2024-03-05 05:45:01 +0000

    sdasd

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,24 @@ function App() {
 
   return (
     <>
+      <AnimatedCursor
+  innerSize={8}
+  outerSize={35}
+  innerScale={1}
+  outerScale={1.7}
+  outerAlpha={0}
+  outerStyle={{
+    border: '3px solid var(--cursor-color)'
+  }}
+  innerStyle={{
+    backgroundColor: 'var(--cursor-color)'
+  }}
+/>
       <Navbar />
       <Hero />
       <Skills/>
       <Services/>
       <Portfolio/>
-      <Number/>
       <Testimonials/>
       <Contact/>
       <Footer/>
