c124a2ec41ea414bd0bb12e190535342c2a1f0b4
oyasuminaaa <150317826+oyasuminaaa@users.noreply.github.com>
2024-03-05 05:34:33 +0000

    66asd

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,27 @@ import Portfolio from './components/Portfolio/Portfolio'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import AnimatedCursor from 'react-animated-cursor'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+        outerStyle={{
+          border: '3px solid var(--cursor-color)'
+        }}
+      />
       <Navbar />
       <Hero />
       <Skills/>
